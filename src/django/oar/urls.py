7548b0dc5cae3cb6a610667c23f5b425cae5849e
Justin Walgran <jwalgran@azavea.com>
2019-01-11 10:15:54 -0700

    Setup Django static file serving
    Ensures that static files for the admin site are served correctly.

--- src/django/oar/urls.py
+++ src/django/oar/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, re_path, include
+
+from oar import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^health-check/', include('watchman.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
