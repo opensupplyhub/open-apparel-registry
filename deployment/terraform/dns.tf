483eadebd1c4c3e292f8967b48b940cdee351cfb
Rocky Breslow <rbreslow@azavea.com>
2019-01-23 16:32:53 -0500

    Add AAAA record

--- deployment/terraform/dns.tf
+++ deployment/terraform/dns.tf
@@ -61,3 +61,15 @@ resource "aws_route53_record" "www" {
     evaluate_target_health = false
   }
 }
+
+resource "aws_route53_record" "www_ipv6" {
+  zone_id = "${aws_route53_zone.external.zone_id}"
+  name    = "${var.r53_public_hosted_zone}"
+  type    = "AAAA"
+
+  alias {
+    name                   = "${aws_cloudfront_distribution.cdn.domain_name}"
+    zone_id                = "${aws_cloudfront_distribution.cdn.hosted_zone_id}"
+    evaluate_target_health = false
+  }
+}
