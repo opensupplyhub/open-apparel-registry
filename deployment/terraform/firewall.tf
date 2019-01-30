643c5ff0ef6f9ff90270c7aaeef57a38c944ad9b
Rocky Breslow <rbreslow@azavea.com>
2019-01-30 13:32:59 -0500

    Refactor security group rules
    * Add Bastion -> ECS security group rules
    * Add Bastion -> WWW security group rules

--- deployment/terraform/firewall.tf
+++ deployment/terraform/firewall.tf
@@ -1,4 +1,6 @@
-# allow all inbound traffic to the bastion on port 22
+#
+# Bastion security group resources
+#
 resource "aws_security_group_rule" "bastion_ingress_ssh" {
   type        = "ingress"
   from_port   = 22
@@ -9,7 +11,6 @@ resource "aws_security_group_rule" "bastion_ingress_ssh" {
   security_group_id = "${module.vpc.bastion_security_group_id}"
 }
 
-# allow all outbound traffic from the bastion to the rds instance
 resource "aws_security_group_rule" "bastion_egress_rds" {
   type      = "egress"
   from_port = "${module.database.port}"
@@ -20,19 +21,42 @@ resource "aws_security_group_rule" "bastion_egress_rds" {
   source_security_group_id = "${module.database.database_security_group_id}"
 }
 
-# allow all inbound traffic from the bastion to the rds instance
-resource "aws_security_group_rule" "rds_bastion_ingress" {
-  type      = "ingress"
-  from_port = "${module.database.port}"
-  to_port   = "${module.database.port}"
+resource "aws_security_group_rule" "bastion_egress_app" {
+  type      = "egress"
+  from_port = "${var.app_port}"
+  to_port   = "${var.app_port}"
   protocol  = "tcp"
 
-  security_group_id        = "${module.database.database_security_group_id}"
-  source_security_group_id = "${module.vpc.bastion_security_group_id}"
+  security_group_id        = "${module.vpc.bastion_security_group_id}"
+  source_security_group_id = "${aws_security_group.app.id}"
 }
 
-# allow all inbound traffic to the load balancer on port 443
-resource "aws_security_group_rule" "app_alb_ingress_https" {
+resource "aws_security_group_rule" "bastion_egress_http" {
+  type             = "egress"
+  from_port        = "80"
+  to_port          = "80"
+  protocol         = "tcp"
+  cidr_blocks      = ["0.0.0.0/0"]
+  ipv6_cidr_blocks = ["::/0"]
+
+  security_group_id = "${module.vpc.bastion_security_group_id}"
+}
+
+resource "aws_security_group_rule" "bastion_egress_https" {
+  type             = "egress"
+  from_port        = "443"
+  to_port          = "443"
+  protocol         = "tcp"
+  cidr_blocks      = ["0.0.0.0/0"]
+  ipv6_cidr_blocks = ["::/0"]
+
+  security_group_id = "${module.vpc.bastion_security_group_id}"
+}
+
+#
+# App ALB security group resources
+#
+resource "aws_security_group_rule" "alb_ingress_https" {
   type             = "ingress"
   from_port        = 443
   to_port          = 443
@@ -43,8 +67,7 @@ resource "aws_security_group_rule" "app_alb_ingress_https" {
   security_group_id = "${aws_security_group.alb.id}"
 }
 
-# allow outbound traffic from the alb to the app on the app port
-resource "aws_security_group_rule" "app_alb_egress_all" {
+resource "aws_security_group_rule" "alb_egress_app" {
   type      = "egress"
   from_port = "${var.app_port}"
   to_port   = "${var.app_port}"
@@ -54,18 +77,43 @@ resource "aws_security_group_rule" "app_alb_egress_all" {
   source_security_group_id = "${aws_security_group.app.id}"
 }
 
-# allow inbound traffic from the alb to the app on the app port
-resource "aws_security_group_rule" "app_ingress_alb" {
+#
+# RDS security group resources
+#
+resource "aws_security_group_rule" "rds_ingress_app" {
   type      = "ingress"
-  from_port = "${var.app_port}"
-  to_port   = "${var.app_port}"
+  from_port = "${module.database.port}"
+  to_port   = "${module.database.port}"
   protocol  = "tcp"
 
-  security_group_id        = "${aws_security_group.app.id}"
-  source_security_group_id = "${aws_security_group.alb.id}"
+  security_group_id        = "${module.database.database_security_group_id}"
+  source_security_group_id = "${aws_security_group.app.id}"
 }
 
-# allow all outbound traffic from the app to the rds instance
+resource "aws_security_group_rule" "rds_ingress_bastion" {
+  type      = "ingress"
+  from_port = "${module.database.port}"
+  to_port   = "${module.database.port}"
+  protocol  = "tcp"
+
+  security_group_id        = "${module.database.database_security_group_id}"
+  source_security_group_id = "${module.vpc.bastion_security_group_id}"
+}
+
+#
+# Container instance security group resources
+#
+resource "aws_security_group_rule" "app_egress_https" {
+  type             = "egress"
+  from_port        = 443
+  to_port          = 443
+  protocol         = "tcp"
+  cidr_blocks      = ["0.0.0.0/0"]
+  ipv6_cidr_blocks = ["::/0"]
+
+  security_group_id = "${aws_security_group.app.id}"
+}
+
 resource "aws_security_group_rule" "app_egress_rds" {
   type      = "egress"
   from_port = "${module.database.port}"
@@ -76,25 +124,22 @@ resource "aws_security_group_rule" "app_egress_rds" {
   source_security_group_id = "${module.database.database_security_group_id}"
 }
 
-# allow all inbound traffic from the app to the rds instance
-resource "aws_security_group_rule" "rds_app_ingress" {
+resource "aws_security_group_rule" "app_ingress_alb" {
   type      = "ingress"
-  from_port = "${module.database.port}"
-  to_port   = "${module.database.port}"
+  from_port = "${var.app_port}"
+  to_port   = "${var.app_port}"
   protocol  = "tcp"
 
-  security_group_id        = "${module.database.database_security_group_id}"
-  source_security_group_id = "${aws_security_group.app.id}"
+  security_group_id        = "${aws_security_group.app.id}"
+  source_security_group_id = "${aws_security_group.alb.id}"
 }
 
-# allow all outbound traffic from the app on port 443
-resource "aws_security_group_rule" "app_egress_https" {
-  type             = "egress"
-  from_port        = 443
-  to_port          = 443
-  protocol         = "tcp"
-  cidr_blocks      = ["0.0.0.0/0"]
-  ipv6_cidr_blocks = ["::/0"]
+resource "aws_security_group_rule" "app_ingress_bastion" {
+  type      = "ingress"
+  from_port = "${var.app_port}"
+  to_port   = "${var.app_port}"
+  protocol  = "tcp"
 
-  security_group_id = "${aws_security_group.app.id}"
+  security_group_id        = "${aws_security_group.app.id}"
+  source_security_group_id = "${module.vpc.bastion_security_group_id}"
 }
