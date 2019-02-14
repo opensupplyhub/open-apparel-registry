ed5a8d9960fde25d5e98ef5b3300d31afd884dec
Kelly Innes <kinnes@azavea.com>
2019-02-14 12:44:07 -0500

    Create tests for facility data
    - check that inactive lists do not have contributor, name, or address
    data returned in the facility data
    - check that non-public lists do not have contributor, name, or address
    data returned in facility data
    - check that only confirmed facility matches are used in facility data
    - check that canonical facility names and addresses are excluded from
    other names and other addresses
    - check that other names, other addresses, and contributors facility
    data returns properly

--- src/django/api/tests.py
+++ src/django/api/tests.py
@@ -523,3 +523,186 @@ class FacilityListItemMatchingTest(TestCase):
         self.assertEqual(match_2.facility, facility_1)
         self.assertEqual(FacilityMatch.AUTOMATIC, match_2.status)
         self.assertEqual(1.0, match_2.confidence)
+
+
+class FacilityNamesAddressesAndContributorsTest(TestCase):
+    def setUp(self):
+        self.name_one = 'name_one'
+        self.name_two = 'name_two'
+        self.address_one = 'address_one'
+        self.address_two = 'address_two'
+        self.email_one = 'one@example.com'
+        self.email_two = 'two@example.com'
+        self.org_one_name = 'organization one'
+        self.org_two_name = 'organization two'
+        self.country_code = 'US'
+        self.list_one_name = 'one'
+        self.list_two_name = 'two'
+        self.user_one = User.objects.create(email=self.email_one)
+        self.user_two = User.objects.create(email=self.email_two)
+
+        self.org_one = Organization \
+            .objects \
+            .create(admin=self.user_one,
+                    name=self.org_one_name,
+                    org_type=Organization.OTHER_ORG_TYPE)
+
+        self.org_two = Organization \
+            .objects \
+            .create(admin=self.user_two,
+                    name=self.org_two_name,
+                    org_type=Organization.OTHER_ORG_TYPE)
+
+        self.list_one = FacilityList \
+            .objects \
+            .create(header="header",
+                    file_name="one",
+                    name=self.list_one_name,
+                    is_active=True,
+                    is_public=True,
+                    organization=self.org_one)
+
+        self.list_item_one = FacilityListItem \
+            .objects \
+            .create(name=self.name_one,
+                    address=self.address_one,
+                    country_code=self.country_code,
+                    facility_list=self.list_one,
+                    row_index=1,
+                    status=FacilityListItem.CONFIRMED_MATCH)
+
+        self.list_two = FacilityList \
+            .objects \
+            .create(header="header",
+                    file_name="two",
+                    name=self.list_two_name,
+                    is_active=True,
+                    is_public=True,
+                    organization=self.org_two)
+
+        self.list_item_two = FacilityListItem \
+            .objects \
+            .create(name=self.name_two,
+                    address=self.address_two,
+                    country_code=self.country_code,
+                    facility_list=self.list_two,
+                    row_index="2",
+                    status=FacilityListItem.CONFIRMED_MATCH)
+
+        self.facility = Facility \
+            .objects \
+            .create(name=self.name_one,
+                    address=self.address_one,
+                    country_code=self.country_code,
+                    location=Point(0, 0),
+                    created_from=self.list_item_one)
+
+        self.facility_match_one = FacilityMatch \
+            .objects \
+            .create(status=FacilityMatch.CONFIRMED,
+                    facility=self.facility,
+                    results="",
+                    facility_list_item=self.list_item_one)
+
+        self.facility_match_two = FacilityMatch \
+            .objects \
+            .create(status=FacilityMatch.CONFIRMED,
+                    facility=self.facility,
+                    results="",
+                    facility_list_item=self.list_item_two)
+
+    def test_returns_contributors(self):
+        contributors = self.facility.contributors()
+        contributor_one = "{} ({})".format(self.org_one_name,
+                                           self.list_one_name)
+        contributor_two = "{} ({})".format(self.org_two_name,
+                                           self.list_two_name)
+        self.assertIn(contributor_one, contributors)
+        self.assertIn(contributor_two, contributors)
+        self.assertEqual(len(contributors), 2)
+
+    def test_excludes_canonical_name_from_other_names(self):
+        other_names = self.facility.other_names()
+        self.assertIn(self.name_two, other_names)
+        self.assertNotIn(self.name_one, other_names)
+
+    def test_excludes_canonical_address_from_other_addresses(self):
+        other_addresses = self.facility.other_addresses()
+        self.assertIn(self.address_two, other_addresses)
+        self.assertNotIn(self.address_one, other_addresses)
+
+    def test_excludes_other_names_from_inactive_lists(self):
+        self.list_two.is_active = False
+        self.list_two.save()
+        other_names = self.facility.other_names()
+        self.assertNotIn(self.name_two, other_names)
+        self.assertEqual(len(other_names), 0)
+
+    def test_excludes_other_addresses_from_inactive_lists(self):
+        self.list_two.is_active = False
+        self.list_two.save()
+        other_addresses = self.facility.other_addresses()
+        self.assertNotIn(self.address_two, other_addresses)
+        self.assertEqual(len(other_addresses), 0)
+
+    def test_excludes_contributors_from_inactive_lists(self):
+        self.list_two.is_active = False
+        self.list_two.save()
+        contributors = self.facility.contributors()
+        contributor_one = "{} ({})".format(self.org_one_name,
+                                           self.list_one_name)
+        contributor_two = "{} ({})".format(self.org_two_name,
+                                           self.list_two_name)
+        self.assertIn(contributor_one, contributors)
+        self.assertNotIn(contributor_two, contributors)
+
+    def test_excludes_other_names_from_non_public_lists(self):
+        self.list_two.is_public = False
+        self.list_two.save()
+        other_names = self.facility.other_names()
+        self.assertNotIn(self.name_two, other_names)
+        self.assertEqual(len(other_names), 0)
+
+    def test_excludes_other_addresses_from_non_public_lists(self):
+        self.list_two.is_public = False
+        self.list_two.save()
+        other_addresses = self.facility.other_addresses()
+        self.assertNotIn(self.address_two, other_addresses)
+        self.assertEqual(len(other_addresses), 0)
+
+    def test_excludes_contributors_from_non_public_lists(self):
+        self.list_two.is_public = False
+        self.list_two.save()
+        contributors = self.facility.contributors()
+        contributor_one = "{} ({})".format(self.org_one_name,
+                                           self.list_one_name)
+        contributor_two = "{} ({})".format(self.org_two_name,
+                                           self.list_two_name)
+        self.assertIn(contributor_one, contributors)
+        self.assertNotIn(contributor_two, contributors)
+
+    def test_excludes_unmatched_facilities_from_other_names(self):
+        self.facility_match_two.status = FacilityMatch.REJECTED
+        self.facility_match_two.save()
+        other_names = self.facility.other_names()
+        self.assertNotIn(self.name_two, other_names)
+        self.assertEqual(len(other_names), 0)
+
+    def test_excludes_unmatched_facilities_from_other_addresses(self):
+        self.facility_match_two.status = FacilityMatch.REJECTED
+        self.facility_match_two.save()
+        other_addresses = self.facility.other_addresses()
+        self.assertNotIn(self.name_one, other_addresses)
+        self.assertEqual(len(other_addresses), 0)
+
+    def test_excludes_unmatched_facilities_from_contributors(self):
+        self.facility_match_two.status = FacilityMatch.REJECTED
+        self.facility_match_two.save()
+        contributors = self.facility.contributors()
+        contributor_one = "{} ({})".format(self.org_one_name,
+                                           self.list_one_name)
+        contributor_two = "{} ({})".format(self.org_two_name,
+                                           self.list_two_name)
+        self.assertIn(contributor_one, contributors)
+        self.assertNotIn(contributor_two, contributors)
+        self.assertEqual(len(contributors), 1)
