6e8e11d88bfebd7257ee1e6254bf6eea0c16c3d5
mwilliams <mwilliams@azavea.com>
2019-03-06 12:32:52 -0500

    fix linting errors on gdrp compoonent

--- src/app/src/components/GDPRNotification.jsx
+++ src/app/src/components/GDPRNotification.jsx
@@ -1,29 +1,25 @@
 import React, { PureComponent } from 'react';
-import Button from './Button';
 import Snackbar from '@material-ui/core/Snackbar';
+import Button from './Button';
 import ShowOnly from './ShowOnly';
 import COLOURS from '../util/COLOURS';
 
 const GDPRActions = (
-  <div>
-    <Button 
-      text="Reject"
-      style={{
-        background: COLOURS.LIGHT_BLUE,
-        marginRight: '10px'
-      }}
-      onClick={this.dismissGDPRAlert}
-    >
-    </Button>
-    <Button 
-      text="Accept"
-      onClick={this.dismissGDPRAlert}
-    >
-    </Button>
-  </div>
+    <div>
+        <Button
+            text="Reject"
+            style={{
+                background: COLOURS.LIGHT_BLUE,
+                marginRight: '10px',
+            }}
+            onClick={this.dismissGDPRAlert}
+        />
+        <Button text="Accept" onClick={this.dismissGDPRAlert} />
+    </div>
 );
 
-const GDPRMessage = "We use cookies to collect and analyze information on site performance and usage, and to enhance content. By clicking 'Accept', you agree to allow cookies to be placed. To find out more, visit our terms of service and our privacy policy.";
+const GDPRMessage =
+    'We use cookies to collect and analyze information on site performance and usage, and to enhance content. By clicking Accept, you agree to allow cookies to be placed. To find out more, visit our terms of service and our privacy policy.';
 
 class GDPRNotification extends PureComponent {
     state = { open: true };
@@ -43,17 +39,16 @@ class GDPRNotification extends PureComponent {
     render() {
         return (
             <ShowOnly when={this.state.open}>
-              <Snackbar
-                className="gdpr-notification"
-                open={this.state.open}
-                anchorOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'right',
-                }}
-                action={GDPRActions}
-                message={GDPRMessage}
-              >
-              </Snackbar>
+                <Snackbar
+                    className="gdpr-notification"
+                    open={this.state.open}
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'right',
+                    }}
+                    action={GDPRActions}
+                    message={GDPRMessage}
+                />
             </ShowOnly>
         );
     }
