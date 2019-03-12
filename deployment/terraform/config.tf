be867c255abe27798eff79623dc95845ccb6a5cd
Rocky Breslow <rbreslow@azavea.com>
2019-03-11 20:02:35 -0400

    Add CloudFront certificate comment

--- deployment/terraform/config.tf
+++ deployment/terraform/config.tf
@@ -3,6 +3,15 @@ provider "aws" {
   version = "~> 1.56.0"
 }
 
+/**
+* To use an ACM Certificate with Amazon CloudFront, you must 
+* request or import the certificate in the US East (N. Virginia) region. 
+* ACM Certificates in this region that are associated with a CloudFront 
+* distribution are distributed to all the geographic locations configured 
+* for that distribution.
+*
+* https://docs.aws.amazon.com/acm/latest/userguide/acm-regions.html
+*/
 provider "aws" {
   alias   = "certificates"
   region  = "us-east-1"
