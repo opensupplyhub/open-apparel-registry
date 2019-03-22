2e47b87bde533447669a0f747b994ad050740a69
Rocky Breslow <rbreslow@azavea.com>
2019-03-22 10:33:44 -0400

    Remove duplicate import

--- src/django/oar/settings.py
+++ src/django/oar/settings.py
@@ -15,11 +15,8 @@ import requests
 
 from django.core.exceptions import ImproperlyConfigured
 
-from django.core.exceptions import ImproperlyConfigured
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
