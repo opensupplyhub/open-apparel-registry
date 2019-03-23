ad2431ca1370d6ded027a24d6585d869688257ac
Rocky Breslow <rbreslow@azavea.com>
2019-03-23 17:30:35 -0400

    Actually, we can default this to 0

--- deployment/terraform/variables.tf
+++ deployment/terraform/variables.tf
@@ -220,7 +220,9 @@ variable "batch_default_ce_min_vcpus" {
   default = "0"
 }
 
-variable "batch_default_ce_desired_vcpus" {}
+variable "batch_default_ce_desired_vcpus" {
+  default = "0"
+}
 
 variable "batch_default_ce_max_vcpus" {
   default = "16"
