1a56aebeb8cc79fd6f91b2818c05fa6ee8dea442
Kelly Innes <kinnes@azavea.com>
2019-03-26 14:55:35 -0400

    WIP

--- src/app/src/util/util.ga.js
+++ src/app/src/util/util.ga.js
@@ -1,3 +1,7 @@
+import env from './env';
+
+const REACT_APP_GOOGLE_ANALYTICS_KEY = 'REACT_APP_GOOGLE_ANALYTICS_KEY';
+
 export const GA_TRACKING = 'GA_TRACKING';
 export const HAS_ACCEPTED_GA_TRACKING = 'HAS_ACCEPTED_GA_TRACKING';
 export const HAS_REJECTED_GA_TRACKING = 'HAS_REJECTED_GA_TRACKING';
@@ -15,10 +19,24 @@ export const rejectGATracking = () => {
     localStorage.setItem(GA_TRACKING, HAS_REJECTED_GA_TRACKING);
 };
 
+export const enableGATracking = () => {
+    const gaKey = env(REACT_APP_GOOGLE_ANALYTICS_KEY);
+
+    if (gaKey) {
+        const disableKey = `ga-disable-${gaKey}`;
+        window[disableKey] = false;
+    }
+
+    if (window.ga) {
+        window.ga('send', 'pageview');
+    }
+};
+
 export const acceptGATracking = () => {
     // If user has already rejected GA tracking, this is a noop
     if (!userHasRejectedGATracking()) {
         localStorage.setItem(GA_TRACKING, HAS_ACCEPTED_GA_TRACKING);
+        enableGATracking();
     }
 };
 
