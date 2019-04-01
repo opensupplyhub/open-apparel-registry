f74debb05e5f621383fc77271f18b9a4e3e66bde
Kelly Innes <kinnes@azavea.com>
2019-04-01 12:56:03 -0400

    Enable Map to crash without crashing the app
    Enable the FacilitiesMap component to crash without causing the entire
    application to crash by placing an error boundary in `MapAndSidebar`
    which renders a new FacilitiesMapErrorMessage component when the map has
    crashed.
    
    Also log errors to window.Rollbar if window.Rollbar exists.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -16,6 +16,8 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ### Fixed
 
+- Add a new error boundary to enable the FacilitiesMap component to crash without crashing the rest of the app [#446](https://github.com/open-apparel-registry/open-apparel-registry/pull/446)
+
 ### Security
 
 ## [2.1.0] - 2019-04-01
--- src/app/src/components/MapAndSidebar.jsx
+++ src/app/src/components/MapAndSidebar.jsx
@@ -1,10 +1,11 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
 
 import FilterSidebar from './FilterSidebar';
 import FacilityDetailsSidebar from './FacilityDetailSidebar';
 import FacilitiesMap from './FacilitiesMap';
+import FacilitiesMapErrorMessage from './FacilitiesMapErrorMessage';
 
 import '../styles/css/Map.css';
 
@@ -12,41 +13,65 @@ import withQueryStringSync from '../util/withQueryStringSync';
 
 import { facilityDetailsRoute } from '../util/constants';
 
-function MapAndSidebar() {
-    return (
-        <Fragment>
-            <Grid container className="map-sidebar-container">
-                <Grid
-                    item
-                    xs={12}
-                    sm={4}
-                    id="panel-container"
-                >
-                    <Switch>
-                        <Route
-                            path={facilityDetailsRoute}
-                            component={FacilityDetailsSidebar}
-                        />
-                        <Route component={FilterSidebar} />
-                    </Switch>
+class MapAndSidebar extends Component {
+    state = { hasError: false };
+
+    static getDerivedStateFromError() {
+        return { hasError: true };
+    }
+
+    componentDidCatch(error) {
+        if (window.Rollbar) {
+            window.Rollbar.error(error);
+        }
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <Grid container className="map-sidebar-container">
+                    <Grid
+                        item
+                        xs={12}
+                        sm={4}
+                        id="panel-container"
+                    >
+                        <Switch>
+                            <Route
+                                path={facilityDetailsRoute}
+                                component={FacilityDetailsSidebar}
+                            />
+                            <Route component={FilterSidebar} />
+                        </Switch>
+                    </Grid>
+                    <Grid
+                        item
+                        xs={12}
+                        sm={8}
+                        style={{ position: 'relative' }}
+                    >
+                        <Switch>
+                            <Route
+                                path={facilityDetailsRoute}
+                                component={
+                                    this.state.hasError
+                                        ? FacilitiesMapErrorMessage
+                                        : FacilitiesMap
+                                }
+                            />
+                            <Route
+                                component={
+                                    this.state.hasError
+                                        ? FacilitiesMapErrorMessage
+                                        : FacilitiesMap
+                                }
+                            />
+                        </Switch>
+                    </Grid>
                 </Grid>
-                <Grid
-                    item
-                    xs={12}
-                    sm={8}
-                    style={{ position: 'relative' }}
-                >
-                    <Switch>
-                        <Route
-                            path={facilityDetailsRoute}
-                            component={FacilitiesMap}
-                        />
-                        <Route component={FacilitiesMap} />
-                    </Switch>
-                </Grid>
-            </Grid>
-        </Fragment>
-    );
+            </Fragment>
+        );
+    }
 }
 
 export default withQueryStringSync(MapAndSidebar);
