0fb24bccb2e9f57c595df2cfbbcbeb7be81fb769
Justin Walgran <jwalgran@azavea.com>
2019-04-01 08:47:38 -0700

    Rework reprocess_geocode_failures to use AWS Batch
    The original version of this management command ran all the processing in the
    context of the command itself. This worked correctly for test data, but the
    process was killed when trying to run it on production-sized data.
    
    The batch processing tasks are designed to only work on items in a specific
    status so we can safely change the status on just the items we need to process
    and then resubmit the entire list to the geocoding and match tasks.
    
    We have added a `skip_parse` option to `submit_jobs` as a convenience since we
    know that we will not have to re-parse any items.

--- src/django/api/management/commands/reprocess_geocode_failures.py
+++ src/django/api/management/commands/reprocess_geocode_failures.py
@@ -3,16 +3,17 @@ from django.db import transaction
 from django.db.models import Count
 
 from api.models import FacilityList, FacilityListItem
-from api.processing import (geocode_facility_list_item,
-                            match_facility_list_items,
-                            save_match_details)
+
+from oar.settings import ENVIRONMENT
+
+from api.aws_batch import submit_jobs
 
 
 class Command(BaseCommand):
     help = ('Find all FacilityListItem objects that are in the '
-            'ERROR_MATCHING status that do not have a '
-            'geocoded_point. Attempt to geocode them again '
-            'and rerun the match process on the related list.')
+            'ERROR_MATCHING status that do not have a ' 'geocoded_point. Set '
+            'their status to PARSED and resubmit them for processing (or '
+            'print the processing commands in development')
 
     def print_status_counts(self):
         statuses = FacilityListItem.objects \
@@ -32,36 +33,27 @@ class Command(BaseCommand):
             geocoded_point=None, status=FacilityListItem.ERROR_MATCHING)
 
         list_id_set = set()
-        geocode_success_count = 0
-        geocode_fail_count = 0
-        self.stdout.write('Geocoding {} items'.format(str(items.count())))
-        for item in items:
-            list_id_set.add(item.facility_list.id)
-            item.status = FacilityListItem.PARSED
-            geocode_facility_list_item(item)
-            item.save()
-            if item.status == FacilityListItem.GEOCODED:
-                geocode_success_count += 1
-            else:
-                geocode_fail_count += 1
-                self.stdout.write(
-                    self.style.ERROR(
-                        '{} {} {} {}'.format(
-                            item.status,
-                            item.id,
-                            item.country_code,
-                            item.address)))
         self.stdout.write(
-            '{} successes {} failures'.format(
-                geocode_success_count, geocode_fail_count))
+            'Changing status of {} items'.format(str(items.count())))
+        with transaction.atomic():
+            for item in items:
+                list_id_set.add(item.facility_list.id)
+                item.status = FacilityListItem.PARSED
+                item.save()
+        self.print_status_counts()
 
         lists = FacilityList.objects \
                             .filter(id__in=list(list_id_set)) \
                             .order_by('id')
-        self.stdout.write('Matching {} lists'.format(str(lists.count())))
-        for facility_list in lists:
-            result = match_facility_list_items(facility_list)
-            with transaction.atomic():
-                save_match_details(result)
 
-        self.print_status_counts()
+        self.stdout.write('Reprocessing {} lists'.format(str(lists.count())))
+        for facility_list in lists:
+            if ENVIRONMENT in ('Staging', 'Production'):
+                job_ids = submit_jobs(ENVIRONMENT, facility_list,
+                                      skip_parse=True)
+                self.stdout.write('{} {}'.format(facility_list.id, job_ids))
+            else:
+                command = ('./scripts/manage batch_process '
+                           '--list-id {} --action {}')
+                self.stdout.write(command.format(facility_list.id, 'geocode'))
+                self.stdout.write(command.format(facility_list.id, 'match'))
