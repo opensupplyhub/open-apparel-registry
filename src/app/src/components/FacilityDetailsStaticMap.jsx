7a6331e7f811a67b129f2f8e9ef60a04765888ca
Justin Walgran <jwalgran@azavea.com>
2019-05-29 15:17:22 -0700

    Load static Google maps from the proper host based on country
    Conditionally sets the host for the static image request based on whether or not
    the client is in China and passes the detected country code to the `region`
    parameter.

--- src/app/src/components/FacilityDetailsStaticMap.jsx
+++ src/app/src/components/FacilityDetailsStaticMap.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bool, string } from 'prop-types';
 
 import { GOOGLE_CLIENT_SIDE_API_KEY } from '../util/constants.facilitiesMap';
 
 import { facilityPropType } from '../util/propTypes';
 
-const baseURL = 'https:///maps.googleapis.com/maps/api/staticmap';
+import { COUNTRY_CODES } from '../util/constants';
+
+import ShowOnly from './ShowOnly';
+
 const staticParams =
-    'zoom=18&size=320x200&maptype=satellite&region=IE';
+    'zoom=18&size=320x200&maptype=satellite';
 const apiKey = `key=${GOOGLE_CLIENT_SIDE_API_KEY}`;
 
-const makeGoogleMapsStaticMapURLFromLatLng = ({ lat, lng }) =>
-    `${baseURL}?center=${lat},${lng}&markers=icon:${window.location.origin}/images/static-selectedmarker.png%7C${lat},${lng}&${staticParams}&${apiKey}`;
+const makeGoogleMapsStaticMapURL = ({ baseURL, countryCode, lat, lng }) =>
+    `${baseURL}?center=${lat},${lng}&markers=icon:${window.location.origin}/images/static-selectedmarker.png%7C${lat},${lng}&region=${countryCode}&${staticParams}&${apiKey}`;
 
-export default function FacilityDetailsStaticMap({
+function FacilityDetailsStaticMap({
     data: {
         geometry: {
             coordinates: [
@@ -24,16 +29,46 @@ export default function FacilityDetailsStaticMap({
             name,
         },
     },
+    clientInfoFetched,
+    baseURL,
+    countryCode,
 }) {
     return (
-        <img
-            src={makeGoogleMapsStaticMapURLFromLatLng({ lat, lng })}
-            alt={`Facility ${name} at latitide ${lat} and longitude ${lng}`}
-            className="facility-detail_map"
-        />
+        <ShowOnly when={clientInfoFetched}>
+            <img
+                src={makeGoogleMapsStaticMapURL(
+                    { baseURL, countryCode, lat, lng },
+                )}
+                alt={`Facility ${name} at latitide ${lat} and longitude ${lng}`}
+                className="facility-detail_map"
+            />
+        </ShowOnly>
     );
 }
 
 FacilityDetailsStaticMap.propTypes = {
     data: facilityPropType.isRequired,
+    clientInfoFetched: bool.isRequired,
+    countryCode: string.isRequired,
+    baseURL: string.isRequired,
 };
+
+function mapStateToProps({
+    clientInfo: {
+        fetched,
+        countryCode,
+    },
+}) {
+    const host = countryCode === COUNTRY_CODES.china
+        ? 'www.google.cn'
+        : 'maps.googleapis.com';
+    const baseURL = `https:///${host}/maps/api/staticmap`;
+
+    return {
+        clientInfoFetched: fetched,
+        countryCode: countryCode || COUNTRY_CODES.default,
+        baseURL,
+    };
+}
+
+export default connect(mapStateToProps)(FacilityDetailsStaticMap);
