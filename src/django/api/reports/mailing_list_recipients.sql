07c3f412ff790d5536df23018bb0901b7c309245
Justin Walgran <jwalgran@azavea.com>
2019-08-12 09:16:20 -0700

    Add contributor mailing list recipient report
    We regularly need to get a list of contributors who have opted into the
    newsletter. Including the unfiltered list of non-public contributors is a
    convenience.

---
+++ src/django/api/reports/mailing_list_recipients.sql
@@ -0,0 +1,12 @@
+SELECT
+  email,
+  name,
+  date(u.created_at) AS registration_date,
+  regexp_replace(description, E'[\\n\\r]+', ' ', 'g' ) AS description,
+  contrib_type,
+  website
+FROM api_user u
+JOIN api_contributor c ON u.id = c.admin_id
+WHERE u.should_receive_newsletter = 't'
+AND u.email NOT LIKE '%openapparel.org%'
+ORDER BY u.created_at, email
