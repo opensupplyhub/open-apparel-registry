62df4a4445f5afdb717faedad11b63badb9fa96e
Justin Walgran <jwalgran@azavea.com>
2019-09-16 10:41:35 -0700

    Add update location dashboard page
    The layout of this page is based on other "two panel" pages, like facility
    merging.
    
    We added "Location" to the `DashboardFacilityCard` so the existing coordinates
    are visible. This adds the location to every dashboard page that uses the
    `DashboardFacilityCard` but showing extra information is not a problem.
    
    The optional notes and contributor fields allow a superuser to record optional
    descriptive detail regarding the change in location and optionally specify the
    contributor that provided the updated coordinates.

---
+++ src/app/src/components/DashboardUpdateFacilityLocationCard.jsx
@@ -0,0 +1,246 @@
+import React from 'react';
+import { arrayOf, bool, func, shape, string } from 'prop-types';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import get from 'lodash/get';
+import round from 'lodash/round';
+import { distance, point } from '@turf/turf';
+import ReactSelect from 'react-select';
+
+import {
+    facilityPropType,
+    contributorOptionsPropType,
+    contributorOptionPropType,
+} from '../util/propTypes';
+
+import FacilityDetailsStaticMap from './FacilityDetailsStaticMap';
+
+const styles = Object.freeze({
+    cardStyles: Object.freeze({
+        width: '45%',
+        margin: '0 20px',
+        padding: '10px',
+    }),
+    textSectionStyles: Object.freeze({
+        padding: '10px 20px',
+    }),
+    cardActionsStyles: Object.freeze({
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '10px 20px',
+    }),
+    cardContentStyles: Object.freeze({
+        display: 'flex',
+        flexDirection: 'column',
+        padding: '0 20px',
+    }),
+    labelStyles: Object.freeze({
+        fontSize: '16px',
+        fontWeight: '700',
+        padding: '5px 0 0',
+    }),
+    fieldStyles: Object.freeze({
+        fontSize: '16px',
+        padding: '0 0 5px',
+    }),
+    listStyles: Object.freeze({
+        margin: '0 5px',
+    }),
+    mapStyles: Object.freeze({
+        padding: '0',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+    }),
+    oarIDStyles: Object.freeze({
+        fontSize: '18px',
+    }),
+    errorStyles: Object.freeze({
+        color: 'red',
+    }),
+    infoContainerStyles: Object.freeze({
+        marging: '10px 0',
+    }),
+    selectStyles: Object.freeze({
+        container: provided =>
+            Object.freeze({
+                ...provided,
+                width: '100%',
+            }),
+    }),
+    optionalFieldsStyles: Object.freeze({
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100%',
+    }),
+    descriptionStyles: Object.freeze({
+        height: '100%',
+        margin: '1rem 0',
+    }),
+});
+
+
+export default function DashboardUpdateLocationCard({
+    newLocation,
+    notes,
+    fetching,
+    error,
+    title,
+    updateLat,
+    updateLng,
+    updateNotes,
+    facility,
+    contributorOptions,
+    contributor,
+    updateContributor,
+}) {
+    const cardContent = (() => {
+        if (fetching) {
+            return <CircularProgress />;
+        }
+
+        if (error && error.length) {
+            return (
+                <ul style={styles.errorStyles}>
+                    {error.map(err => (
+                        <li key={err}>
+                            <span
+                                style={styles.errorStyles}
+                            >
+                                {err}
+                            </span>
+                        </li>
+                    ))}
+                </ul>
+            );
+        }
+
+        const newFacility =
+              facility &&
+              newLocation &&
+              !Number.isNaN(parseFloat(newLocation.lat)) &&
+              !Number.isNaN(parseFloat(newLocation.lng)) &&
+              Object.assign({}, facility, {
+                  geometry: {
+                      type: 'Point',
+                      coordinates: [
+                          parseFloat(newLocation.lng),
+                          parseFloat(newLocation.lat),
+                      ],
+                  },
+              });
+
+        const distanceBetweenLocations = newFacility &&
+              distance(
+                  point(facility.geometry.coordinates),
+                  point(newFacility.geometry.coordinates),
+              );
+
+        return (
+            <>
+                {
+                    newFacility && (
+                        <>
+                            <div style={styles.mapStyles}>
+                                <FacilityDetailsStaticMap data={newFacility} />
+                            </div>
+                            <div style={styles.infoContainerStyles}>
+                                <Typography style={styles.labelStyles}>
+                                    Distance From Current Location
+                                </Typography>
+                                <Typography style={styles.fieldStyles}>
+                                    {`${round(distanceBetweenLocations, 2)} km`}
+                                </Typography>
+                            </div>
+                        </>
+                    )
+                }
+            </>
+        );
+    })();
+
+    return (
+        <>
+            <Card style={styles.cardStyles}>
+                <Typography
+                    variant="title"
+                    style={styles.textSectionStyles}
+                >
+                    {title}
+                </Typography>
+                <CardActions>
+                    <TextField
+                        variant="outlined"
+                        placeholder="Longitude"
+                        value={get(newLocation, 'lng', '') || ''}
+                        onChange={updateLng}
+                    />
+                    <TextField
+                        variant="outlined"
+                        placeholder="Latitude"
+                        value={get(newLocation, 'lat', '') || ''}
+                        onChange={updateLat}
+                    />
+                </CardActions>
+                <CardContent style={styles.cardContentStyles}>
+                    {cardContent}
+                </CardContent>
+                <CardActions>
+                    <div style={styles.optionalFieldsStyles}>
+                        <ReactSelect
+                            styles={styles.selectStyles}
+                            isClearable
+                            id="contributors"
+                            name="contributors"
+                            placeholder="Contributor (optional)"
+                            className="basic-multi-select notranslate"
+                            classNamePrefix="select"
+                            options={contributorOptions}
+                            value={contributor}
+                            onChange={updateContributor}
+                        />
+                        <TextField
+                            variant="outlined"
+                            multiline
+                            rows={20}
+                            value={notes || ''}
+                            placeholder="Notes (optional)"
+                            onChange={updateNotes}
+                            style={styles.descriptionStyles}
+                        />
+                    </div>
+                </CardActions>
+            </Card>
+        </>
+    );
+}
+
+DashboardUpdateLocationCard.defaultProps = {
+    error: null,
+    facility: null,
+    contributor: null,
+};
+
+DashboardUpdateLocationCard.propTypes = {
+    fetching: bool.isRequired,
+    error: arrayOf(string),
+    title: string.isRequired,
+    facility: facilityPropType,
+    notes: string.isRequired,
+    newLocation: shape({
+        lat: string,
+        lng: string,
+    }).isRequired,
+    updateLat: func.isRequired,
+    updateLng: func.isRequired,
+    updateNotes: func.isRequired,
+    contributorOptions: contributorOptionsPropType.isRequired,
+    contributor: contributorOptionPropType,
+    updateContributor: func.isRequired,
+};
---
+++ src/app/src/reducers/UpdateFacilityLocationReducer.js
@@ -0,0 +1,125 @@
+import { createReducer } from 'redux-act';
+import update from 'immutability-helper';
+
+import {
+    startFetchDashboardUpdateLocationContributors,
+    failFetchDashboardUpdateLocationContributors,
+    completeFetchDashboardUpdateLocationContributors,
+    startFetchUpdateLocationFacility,
+    failFetchUpdateLocationFacility,
+    completeFetchUpdateLocationFacility,
+    clearUpdateLocationFacility,
+    updateUpdateLocationFacilityOARID,
+    updateUpdateLocationLat,
+    updateUpdateLocationLng,
+    updateUpdateLocationNotes,
+    updateUpdateLocationContributor,
+    startUpdateFacilityLocation,
+    failUpdateFacilityLocation,
+    completeUpdateFacilityLocation,
+} from '../actions/updateFacilityLocation';
+
+const initialState = Object.freeze({
+    oarID: '',
+    data: null,
+    fetching: false,
+    error: null,
+    notes: '',
+    newLocation: Object.freeze({
+        lat: null,
+        lng: null,
+    }),
+    update: Object.freeze({
+        fetching: false,
+        error: null,
+    }),
+    contributors: Object.freeze({
+        data: Object.freeze([]),
+        fetching: false,
+        error: null,
+    }),
+    contributor: null,
+});
+
+export default createReducer({
+    [startFetchDashboardUpdateLocationContributors]: state => update(state, {
+        contributors: {
+            fetching: { $set: true },
+            error: { $set: null },
+        },
+    }),
+    [failFetchDashboardUpdateLocationContributors]: (state, payload) => update(state, {
+        contributors: {
+            fetching: { $set: false },
+            error: { $set: payload },
+        },
+    }),
+    [completeFetchDashboardUpdateLocationContributors]: (state, payload) => update(state, {
+        contributors: {
+            fetching: { $set: false },
+            error: { $set: null },
+            data: { $set: payload },
+        },
+    }),
+    [startFetchUpdateLocationFacility]: state => update(state, {
+        fetching: { $set: true },
+        error: { $set: initialState.error },
+        data: { $set: initialState.data },
+    }),
+    [failFetchUpdateLocationFacility]: (state, error) => update(state, {
+        fetching: { $set: initialState.fetching },
+        error: { $set: error },
+    }),
+    [completeFetchUpdateLocationFacility]: (state, data) => update(state, {
+        fetching: { $set: initialState.fetching },
+        data: { $set: data },
+    }),
+    [clearUpdateLocationFacility]: state => update(state, {
+        oarID: { $set: initialState.oarID },
+        data: { $set: initialState.data },
+        fetching: { $set: initialState.fetching },
+        error: { $set: initialState.error },
+    }),
+    [updateUpdateLocationFacilityOARID]: (state, oarID) => update(state, {
+        oarID: { $set: oarID },
+        error: { $set: initialState.error },
+    }),
+    [updateUpdateLocationLat]: (state, lat) => update(state, {
+        newLocation: {
+            lat: { $set: lat },
+        },
+    }),
+    [updateUpdateLocationLng]: (state, lng) => update(state, {
+        newLocation: {
+            lng: { $set: lng },
+        },
+    }),
+    [updateUpdateLocationNotes]: (state, notes) => update(state, {
+        notes: { $set: notes },
+    }),
+    [startUpdateFacilityLocation]: state => update(state, {
+        update: {
+            fetching: { $set: true },
+            error: { $set: initialState.update.error },
+        },
+    }),
+    [completeUpdateFacilityLocation]: (state, data) => update(state, {
+        update: {
+            fetching: { $set: initialState.update.fetching },
+            error: { $set: initialState.update.error },
+        },
+        newLocation: { $set: initialState.newLocation },
+        notes: { $set: initialState.notes },
+        contributor: { $set: initialState.contributor },
+        data: { $set: data },
+    }),
+    [failUpdateFacilityLocation]: (state, error) => update(state, {
+        update: {
+            fetching: { $set: initialState.update.fetching },
+            error: { $set: error },
+        },
+    }),
+    [updateUpdateLocationContributor]: (state, contributor) => update(state, {
+        contributor: { $set: contributor },
+    }),
+}, initialState);
