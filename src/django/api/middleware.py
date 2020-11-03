b6072e34e8308eab5d0d799a29c26a3369dbc90b
Tai Wilkin <twilkin@azavea.com>
2020-11-03 13:34:05 -0500

    Add RequestMeter middleware
    Adds RequestMeter middleware that returns 402 if a request was made
    with a token and there is an active, unexpired ApiBlock record
    associated with the contributor.

--- src/django/api/middleware.py
+++ src/django/api/middleware.py
@@ -1,11 +1,15 @@
 import sys
+import datetime
 
+from django.utils import timezone
 from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
 
 from rest_framework.authentication import get_authorization_header
-
+from django.http import HttpResponse
 
 from api.models import RequestLog
+from api.limits import get_api_block
 
 
 def _report_error_to_rollbar(request, auth):
@@ -44,3 +48,33 @@ class RequestLogMiddleware:
                 pass  # We don't want this logging middleware to fail a request
 
         return response
+
+
+def has_active_block(request):
+    try:
+        if request.user and request.user.is_authenticated:
+            auth = get_authorization_header(request)
+            if auth and auth.split()[0].lower() == 'token'.encode():
+                contributor = request.user.contributor
+                apiBlock = get_api_block(contributor)
+                at_datetime = datetime.datetime.now(tz=timezone.utc)
+                return (apiBlock is not None and
+                        apiBlock.until > at_datetime and apiBlock.active)
+    except ObjectDoesNotExist:
+        return False
+    return False
+
+
+class RequestMeterMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        is_docs = request.get_full_path() == '/api/docs/?format=openapi'
+        is_blocked = has_active_block(request)
+
+        if is_blocked and not is_docs:
+            return HttpResponse('API limit exceeded', status=402)
+        else:
+            response = self.get_response(request)
+            return response
