70f18e012e7bf82ec921f979729d9105c878c9ac
Cole Kettler <me@colekettler.com>
2021-01-12 09:16:40 -0500

    Use S3 bucket grants to fix CloudFront logging

--- deployment/terraform/cdn.tf
+++ deployment/terraform/cdn.tf
@@ -85,7 +85,7 @@ resource "aws_cloudfront_distribution" "cdn" {
 
   logging_config {
     include_cookies = false
-    bucket          = "${aws_s3_bucket.logs.id}.s3.amazonaws.com"
+    bucket          = "${aws_s3_bucket.logs.bucket_domain_name}"
     prefix          = "CDN"
   }
 
