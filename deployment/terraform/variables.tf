f59a4410b2bc120d8805e04ab6bc77e44748f51d
Cole Kettler <me@colekettler.com>
2021-01-12 14:40:10 -0500

    Fix S3 permissions to allow CloudFront logging (#1208)
    * Use S3 bucket grants to fix CloudFront logging
    
    * Update CHANGELOG with fix for CloudFront logging grants

--- deployment/terraform/variables.tf
+++ deployment/terraform/variables.tf
@@ -273,3 +273,6 @@ variable "aws_lambda_service_role_policy_arn" {
 
 variable "oar_client_key" {}
 
+variable "aws_cloudfront_canonical_user_id" {
+  default = "c4c1ede66af53448b93c283ce9448c4ba468c9432aa01d700d3878632f77d2d0"
+}
