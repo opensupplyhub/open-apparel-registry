1410a9abca1d27a2f722ba653bb3f2dfbd4dd0a4
Cole Kettler <me@colekettler.com>
2021-04-19 16:31:06 -0400

    Parameterize gunicorn worker timeout, default to 3 minutes

--- deployment/terraform/container_service.tf
+++ deployment/terraform/container_service.tf
@@ -120,7 +120,8 @@ data "template_file" "app" {
     postgres_password = "${var.rds_database_password}"
     postgres_db       = "${var.rds_database_name}"
 
-    gunicorn_workers = 1
+    gunicorn_workers        = 1
+    gunicorn_worker_timeout = "${var.gunicorn_worker_timeout}"
 
     google_server_side_api_key = "${var.google_server_side_api_key}"
     google_client_side_api_key = "${var.google_client_side_api_key}"
