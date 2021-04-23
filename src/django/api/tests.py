3aeb0d9feaabea570e1bc8593205eca6e2fd7bb7
Tai Wilkin-Corraggio <twilkin@azavea.com>
2021-04-23 11:56:00 -0400

    Add API to get non-standard fields for contributor
    Returns a list of all the field names that a contributor has submitted
    either by API or list, minus our standard fields (country, name,
    address, ppe_*, lat, lng).
    
    Fields are parsed from list headers and from the keys of single-facility
    API submissions.

--- src/django/api/tests.py
+++ src/django/api/tests.py
@@ -6369,3 +6369,128 @@ class ApiLimitTest(TestCase):
                      contributor=self.contrib_two)
         self.assertEqual(notice_two.api_limit_exceeded_sent_on,
                          self.notification_time)
+
+
+class NonstandardFieldsApiTest(APITestCase):
+    def setUp(self):
+        self.url = reverse('nonstandard-fields-list')
+        self.user_email = 'test@example.com'
+        self.user_password = 'example123'
+        self.user = User.objects.create(email=self.user_email)
+        self.user.set_password(self.user_password)
+        self.user.save()
+
+        self.contributor = Contributor \
+            .objects \
+            .create(admin=self.user,
+                    name='test contributor 1',
+                    contrib_type=Contributor.OTHER_CONTRIB_TYPE)
+
+        self.list = FacilityList \
+            .objects \
+            .create(header='country,name,address,extra_1',
+                    file_name='one',
+                    name='First List')
+
+        self.list_source = Source \
+            .objects \
+            .create(facility_list=self.list,
+                    source_type=Source.LIST,
+                    is_active=True,
+                    is_public=True,
+                    contributor=self.contributor)
+
+        self.list_item = FacilityListItem \
+            .objects \
+            .create(name='Item',
+                    address='Address',
+                    country_code='US',
+                    row_index=1,
+                    geocoded_point=Point(0, 0),
+                    status=FacilityListItem.CONFIRMED_MATCH,
+                    source=self.list_source)
+
+        self.api_source = Source \
+            .objects \
+            .create(source_type=Source.SINGLE,
+                    is_active=True,
+                    is_public=True,
+                    contributor=self.contributor)
+
+        self.api_list_item = FacilityListItem \
+            .objects \
+            .create(name='Item',
+                    address='Address',
+                    country_code='US',
+                    raw_data=("{'country': 'US', 'name': 'Item'," +
+                              "'address': 'Address', 'extra_2': 'data'}"),
+                    row_index=1,
+                    geocoded_point=Point(0, 0),
+                    status=FacilityListItem.CONFIRMED_MATCH,
+                    source=self.api_source)
+
+    def test_nonstandard_fields(self):
+        self.client.login(email=self.user_email,
+                          password=self.user_password)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        content = json.loads(response.content)
+        self.assertTrue(2, len(content))
+        self.assertIn('extra_1', content)
+        self.assertIn('extra_2', content)
+
+    def test_doublequote_header(self):
+        self.list.header = '"country","name","address","extra_1"'
+        self.list.save()
+
+        self.client.login(email=self.user_email,
+                          password=self.user_password)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        content = json.loads(response.content)
+        self.assertIn('extra_1', content)
+
+    def test_escaped_singlequote_in_api_data(self):
+        self.api_list_item.raw_data = ("{'country': 'US', 'name': 'Item', " +
+                                       "'address': 'Address', " +
+                                       "'extra_2': 'd\'ataé'}")
+        self.api_list_item.save()
+
+        self.client.login(email=self.user_email,
+                          password=self.user_password)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        content = json.loads(response.content)
+        # We plan to only store raw data as CSV or JSON in the future. Legacy
+        # data can be ignored
+        self.assertNotIn('extra_2', content)
+
+    def test_json_in_api_data(self):
+        self.api_list_item.raw_data = ('{"country": "US", "name": "Item", ' +
+                                       '"address": "Address", "extra_2": ' +
+                                       '"d\'ataé"}')
+        self.api_list_item.save()
+
+        self.client.login(email=self.user_email,
+                          password=self.user_password)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        content = json.loads(response.content)
+        self.assertIn('extra_2', content)
+
+    def test_querydict_in_api_data(self):
+        # Production data at the time this test was written includes a mix of
+        # strigified dicts and stringified QueryDicts
+        self.api_list_item.raw_data = ("<QueryDict: {'name': ['Item'], " +
+                                       "'country': ['US'], 'address': " +
+                                       "['Address'], 'extra_2': ['data']}>")
+        self.api_list_item.save()
+
+        self.client.login(email=self.user_email,
+                          password=self.user_password)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        content = json.loads(response.content)
+        # We plan to only store raw data as CSV or JSON in the future. Legacy
+        # data can be ignored
+        self.assertNotIn('extra_2', content)
