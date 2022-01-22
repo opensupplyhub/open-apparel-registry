e2ce282b2c4e62f62ad15fa817d1bfe9fc45fcb8
Justin Walgran <jwalgran@azavea.com>
2022-01-21 17:25:28 -0700

    Add ContributorManager with filter_by_name method
    To support fuzzy matching contributors by a submitted parent company extended
    field we add a custom manager to the Contributor model.
    
    We set the TRIGRAM_SIMILARY_THRESHOLD by trial and error testing with
    development data.
    
    In the real world multiple people register accounts with the same contributor
    name to we prioritize matches to a verified account, then to one under which
    data has been submitted.

--- src/django/api/tests.py
+++ src/django/api/tests.py
@@ -33,7 +33,8 @@ from api.models import (Facility, FacilityList, FacilityListItem,
                         RequestLog, DownloadLog, FacilityLocation, Source,
                         ApiLimit, ApiBlock, ContributorNotifications,
                         EmbedConfig, EmbedField, NonstandardField,
-                        FacilityActivityReport, ExtendedField)
+                        FacilityActivityReport, ExtendedField,
+                        ContributorManager)
 from api.oar_id import make_oar_id, validate_oar_id
 from api.matching import match_facility_list_items, GazetteerCache
 from api.processing import (parse_facility_list_item,
@@ -7273,3 +7274,106 @@ class ContributorFieldsApiTest(APITestCase):
         self.assertEquals(None, field['value'])
         self.assertEquals('ExtraTwo', field_two['label'])
         self.assertEquals(None, field_two['value'])
+
+
+class ContributorManagerTest(TestCase):
+    fixtures = ['users', 'contributors']
+
+    def test_filter_by_name(self):
+        matches = Contributor.objects.filter_by_name('factory a')
+        self.assertGreater(matches.count(), 0)
+        self.assertEquals(1.0, matches[0].similarity)
+
+        matches = Contributor.objects.filter_by_name('factory')
+        self.assertGreater(matches.count(), 0)
+        self.assertLess(matches[0].similarity, 1.0)
+        self.assertGreater(matches[0].similarity,
+                           ContributorManager.TRIGRAM_SIMILARITY_THRESHOLD)
+
+    def test_filter_by_name_verified(self):
+        user1 = User.objects.create(email='test1@test.com')
+        user2 = User.objects.create(email='test2@test.com')
+        c1 = Contributor \
+            .objects \
+            .create(admin=user1,
+                    name='TESTING',
+                    contrib_type=Contributor.OTHER_CONTRIB_TYPE)
+        c2 = Contributor \
+            .objects \
+            .create(admin=user2,
+                    name='TESTING',
+                    contrib_type=Contributor.OTHER_CONTRIB_TYPE)
+
+        matches = Contributor.objects.filter_by_name('TESTING')
+        self.assertEqual(2, matches.count())
+        # When the names are the same and neither is verified than the second
+        # contributor happens to sort first
+        self.assertEqual(c2, matches[0])
+
+        c1.is_verified = True
+        c1.save()
+        matches = Contributor.objects.filter_by_name('TESTING')
+        self.assertEqual(2, matches.count())
+        # Marking c1 as verified forces it to sort first
+        self.assertEqual(c1, matches[0])
+
+    def test_filter_by_name_source(self):
+        user1 = User.objects.create(email='test1@test.com')
+        user2 = User.objects.create(email='test2@test.com')
+        c1 = Contributor \
+            .objects \
+            .create(admin=user1,
+                    name='TESTING',
+                    contrib_type=Contributor.OTHER_CONTRIB_TYPE)
+        c2 = Contributor \
+            .objects \
+            .create(admin=user2,
+                    name='TESTING',
+                    contrib_type=Contributor.OTHER_CONTRIB_TYPE)
+
+        matches = Contributor.objects.filter_by_name('TESTING')
+        self.assertEqual(2, matches.count())
+        # When the names are the same and neither is verified than the second
+        # contributor happens to sort first
+        self.assertEqual(c2, matches[0])
+
+        Source \
+            .objects \
+            .create(source_type=Source.SINGLE,
+                    is_active=True,
+                    is_public=True,
+                    contributor=c1)
+
+        matches = Contributor.objects.filter_by_name('TESTING')
+        self.assertEqual(2, matches.count())
+        # An active source forces it to sort first
+        self.assertEqual(c1, matches[0])
+
+    def test_filter_by_name_verified_and_source(self):
+        user1 = User.objects.create(email='test1@test.com')
+        user2 = User.objects.create(email='test2@test.com')
+        c1 = Contributor \
+            .objects \
+            .create(admin=user1,
+                    name='TESTING',
+                    contrib_type=Contributor.OTHER_CONTRIB_TYPE)
+        c2 = Contributor \
+            .objects \
+            .create(admin=user2,
+                    name='TESTING',
+                    contrib_type=Contributor.OTHER_CONTRIB_TYPE)
+
+        Source \
+            .objects \
+            .create(source_type=Source.SINGLE,
+                    is_active=True,
+                    is_public=True,
+                    contributor=c1)
+
+        c2.is_verified = True
+        c2.save()
+
+        matches = Contributor.objects.filter_by_name('TESTING')
+        self.assertEqual(2, matches.count())
+        # A verified contributor sorts before one with a source
+        self.assertEqual(c2, matches[0])
