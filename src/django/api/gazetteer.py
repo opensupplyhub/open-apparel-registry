4c8fb949bb574bf737f889131162f58205ebf595
Justin Walgran <jwalgran@azavea.com>
2022-04-21 07:39:12 -0700

    WIP dabase backed gaz

--- src/django/api/gazetteer.py
+++ src/django/api/gazetteer.py
@@ -1,8 +1,9 @@
+import itertools
 import sqlite3
 import dedupe
 from dedupe.api import Link, GazetteerMatching
-from dedupe._typing import Data
-from django.db import connection
+from dedupe._typing import Data, Blocks
+from django.db import connection, transaction
 
 
 class OgrGazetteerMatching(GazetteerMatching):
@@ -33,6 +34,8 @@ class OgrGazetteerMatching(GazetteerMatching):
                         UNIQUE(block_key, record_id))
                     '''.format(id_type=id_type))
 
+
+        ## NEW DB CODE
         # TODO: Should this be a model?
         with connection.cursor() as cursor:
             cursor.execute(
@@ -46,6 +49,7 @@ class OgrGazetteerMatching(GazetteerMatching):
         con.executemany("REPLACE INTO indexed_records VALUES (?, ?)",
                         self.fingerprinter(data.items(), target=True))
 
+        ## NEW DB CODE
         # TODO: Do this in chunks rather than one at a time
         # Can you bulk copy with upsert logic?
         # The dedupee PG example has a bulk insert but this may not do an update
@@ -75,6 +79,7 @@ class OgrGazetteerMatching(GazetteerMatching):
                        (block_key, record_id)''')
         con.execute('''ANALYZE''')
 
+        ## NEW DB CODE
         with connection.cursor() as cursor:
             cursor.execute(
                     """
@@ -89,6 +94,148 @@ class OgrGazetteerMatching(GazetteerMatching):
         con.close()
 
         self.indexed_data.update(data)
+
+    def unindex(self, data: Data) -> None:  # pragma: no cover
+        """
+        Remove records from the index of records to match against.
+        Args:
+            data: a dictionary of records where the keys
+                  are record_ids and the values are
+                  dictionaries with the keys being
+                  field_names
+        """
+
+        for field in self.fingerprinter.index_fields:
+            self.fingerprinter.unindex({record[field]
+                                        for record
+                                        in data.values()},
+                                       field)
+
+        con = sqlite3.connect(self.db)
+        con.executemany('''DELETE FROM indexed_records
+                           WHERE record_id = ?''',
+                        ((k, ) for k in data.keys()))
+
+        ## NEW DB CODE
+        # TODO: Delete in batches?
+        with connection.cursor() as cursor:
+            for k in data.keys():
+                cursor.execute(
+                    """
+                    DELETE FROM dedupe_indexed_records
+                    WHERE record_id = %s
+                    """, k
+                )
+
+        con.commit()
+        con.close()
+
+        for k in data:
+            del self.indexed_data[k]
+
+    def blocks(self, data: Data) -> Blocks:
+        """
+        Yield groups of pairs of records that share fingerprints.
+        Each group contains one record from data_1 paired with the records
+        from the indexed records that data_1 shares a fingerprint with.
+        Each pair within and among blocks will occur at most once. If
+        you override this method, you need to take care to ensure that
+        this remains true, as downstream methods, particularly
+        :func:`many_to_n`, assumes that every pair of records is compared no
+        more than once.
+        Args:
+            data: Dictionary of records, where the keys are record_ids
+                  and the values are dictionaries with the keys being
+                  field names
+        .. code:: python
+            > pairs = matcher.pairs(data)
+            > print(list(pairs))
+            [[((1, {'name' : 'Pat', 'address' : '123 Main'}),
+               (8, {'name' : 'Pat', 'address' : '123 Main'})),
+              ((1, {'name' : 'Pat', 'address' : '123 Main'}),
+               (9, {'name' : 'Sam', 'address' : '123 Main'}))
+              ],
+             [((2, {'name' : 'Sam', 'address' : '2600 State'}),
+               (5, {'name' : 'Pam', 'address' : '2600 Stat'})),
+              ((2, {'name' : 'Sam', 'address' : '123 State'}),
+               (7, {'name' : 'Sammy', 'address' : '123 Main'}))
+             ]]
+        """
+
+        # id_type = dedupe.core.sqlite_id_type(data)
+
+        # con = sqlite3.connect(self.db, check_same_thread=False)
+
+        # con.execute('BEGIN')
+
+        # con.execute('''CREATE TEMPORARY TABLE blocking_map
+        #                (block_key text, record_id {id_type})
+        #             '''.format(id_type=id_type))
+        # con.executemany("INSERT INTO blocking_map VALUES (?, ?)",
+        #                 self.fingerprinter(data.items()))
+
+
+        #https://thebuild.com/blog/2010/12/14/using-server-side-postgresql-cursors-in-django/
+        # This is required to populate the connection object properly
+        # if connection.connection is None:
+        #     cursor = connection.cursor()
+
+        with connection.cursor() as cursor:
+            cursor.execute(
+                """CREATE TEMP TABLE dedupe_blocking_map
+                (block_key text,
+                record_id varchar(32))
+                """
+            )
+            # TODO: Insert in bulk/batcehs?
+            for item in self.fingerprinter(data.items()):
+                cursor.execute("""
+                INSERT INTO dedupe_blocking_map (block_key, record_id) VALUES
+                (%s, %s)
+                """, item)
+
+            cursor.execute('''SELECT DISTINCT a.record_id, b.record_id
+                               FROM dedupe_blocking_map a
+                               INNER JOIN dedupe_indexed_records b
+                               USING (block_key)
+                               ORDER BY a.record_id''')
+
+            # https://code.activestate.com/recipes/137270-use-generators-for-fetching-large-db-record-sets/
+            def ResultIter(c, arraysize=1000):
+                'An iterator that uses fetchmany to keep memory usage down'
+                while True:
+                    results = c.fetchmany(arraysize)
+                    if not results:
+                        break
+                    for result in results:
+                        yield result
+
+            db_pair_blocks = itertools.groupby(ResultIter(cursor), lambda x: x[0])
+            for _, pair_block in db_pair_blocks:
+                yield [((a_record_id, data[a_record_id]),
+                        (b_record_id, self.indexed_data[b_record_id]))
+                       for a_record_id, b_record_id
+                       in pair_block]
+
+        # pairs = con.execute('''SELECT DISTINCT a.record_id, b.record_id
+        #                        FROM blocking_map a
+        #                        INNER JOIN indexed_records b
+        #                        USING (block_key)
+        #                        ORDER BY a.record_id''')
+
+        # pair_blocks = itertools.groupby(pairs,
+        #                                 lambda x: x[0])
+
+        # for _, pair_block in pair_blocks:
+
+        #     yield [((a_record_id, data[a_record_id]),
+        #             (b_record_id, self.indexed_data[b_record_id]))
+        #            for a_record_id, b_record_id
+        #            in pair_block]
+
+        # pairs.close()
+        # con.execute("ROLLBACK")
+        # con.close()
 
 
 class OgrGazetteer(Link, OgrGazetteerMatching):
