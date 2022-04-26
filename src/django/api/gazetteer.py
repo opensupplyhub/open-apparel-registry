5777a82e844667916af1a503292710ea29c26990
Justin Walgran <jwalgran@azavea.com>
2022-04-26 10:14:40 -0700

    WIP remove lint

--- src/django/api/gazetteer.py
+++ src/django/api/gazetteer.py
@@ -1,9 +1,7 @@
 import itertools
-import sqlite3
-import dedupe
 from dedupe.api import Link, GazetteerMatching
 from dedupe._typing import Data, Blocks
-from django.db import connection, transaction
+from django.db import connection
 
 
 class OgrGazetteerMatching(GazetteerMatching):
@@ -31,9 +29,9 @@ class OgrGazetteerMatching(GazetteerMatching):
                 """
             )
 
-        # TODO: Do this in chunks rather than one at a time
-        # Can you bulk copy with upsert logic?
-        # The dedupee PG example has a bulk insert but this may not do an update
+        # TODO: Do this in chunks rather than one at a time Can you bulk copy
+        # with upsert logic? The dedupee PG example has a bulk insert but this
+        # may not do an update
         # https://github.com/dedupeio/dedupe-examples/blob/a767fa5a127bd56afc574c66895bb9b783152e04/pgsql_big_dedupe_example/pgsql_big_dedupe_example.py#L301-L306
         #
         # This SO answer has a "bulk upsert with lock" section that looks
@@ -50,18 +48,18 @@ class OgrGazetteerMatching(GazetteerMatching):
                     INSERT INTO dedupe_indexed_records (block_key, record_id)
                     VALUES (%s, %s)
                     ON CONFLICT (block_key, record_id) DO NOTHING
-                    """, item
+                    """,
+                    item,
                 )
-
 
         with connection.cursor() as cursor:
             cursor.execute(
-                    """
+                """
                     CREATE INDEX IF NOT EXISTS
                     dedupe_indexed_records_idx
                     ON dedupe_indexed_records (block_key, record_id)
                     """
-                )
+            )
 
         # TODO: Remove after `blocks` no longer needs it
         self.indexed_data.update(data)
@@ -77,10 +75,9 @@ class OgrGazetteerMatching(GazetteerMatching):
         """
 
         for field in self.fingerprinter.index_fields:
-            self.fingerprinter.unindex({record[field]
-                                        for record
-                                        in data.values()},
-                                       field)
+            self.fingerprinter.unindex(
+                {record[field] for record in data.values()}, field
+            )
 
         # TODO: Delete in batches?
         with connection.cursor() as cursor:
@@ -89,7 +86,8 @@ class OgrGazetteerMatching(GazetteerMatching):
                     """
                     DELETE FROM dedupe_indexed_records
                     WHERE record_id = %s
-                    """, k
+                    """,
+                    k,
                 )
 
         # TODO: Remove after `blocks` no longer needs it
@@ -126,7 +124,7 @@ class OgrGazetteerMatching(GazetteerMatching):
         """
 
         with connection.cursor() as cursor:
-            cursor.execute('DROP TABLE IF EXISTS dedupe_blocking_map')
+            cursor.execute("DROP TABLE IF EXISTS dedupe_blocking_map")
             cursor.execute(
                 """CREATE TEMP TABLE dedupe_blocking_map
                 (block_key text,
@@ -135,16 +133,21 @@ class OgrGazetteerMatching(GazetteerMatching):
             )
             # TODO: Insert in bulk/batcehs?
             for item in self.fingerprinter(data.items()):
-                cursor.execute("""
+                cursor.execute(
+                    """
                 INSERT INTO dedupe_blocking_map (block_key, record_id) VALUES
                 (%s, %s)
-                """, item)
+                """,
+                    item,
+                )
 
-            cursor.execute('''SELECT DISTINCT a.record_id, b.record_id
+            cursor.execute(
+                """SELECT DISTINCT a.record_id, b.record_id
                                FROM dedupe_blocking_map a
                                INNER JOIN dedupe_indexed_records b
                                USING (block_key)
-                               ORDER BY a.record_id''')
+                               ORDER BY a.record_id"""
+            )
 
             # https://code.activestate.com/recipes/137270-use-generators-for-fetching-large-db-record-sets/
             def ResultIter(c, arraysize=1000):
@@ -156,13 +159,16 @@ class OgrGazetteerMatching(GazetteerMatching):
                     for result in results:
                         yield result
 
-            db_pair_blocks = itertools.groupby(ResultIter(cursor), lambda x: x[0])
+            db_pair_blocks = itertools.groupby(ResultIter(cursor),
+                                               lambda x: x[0])
             for _, pair_block in db_pair_blocks:
-                yield [((a_record_id, data[a_record_id]),
-                        (b_record_id, self.indexed_data[b_record_id]))
-                       for a_record_id, b_record_id
-                       in pair_block]
-
+                yield [
+                    (
+                        (a_record_id, data[a_record_id]),
+                        (b_record_id, self.indexed_data[b_record_id]),
+                    )
+                    for a_record_id, b_record_id in pair_block
+                ]
 
 
 class OgrGazetteer(Link, OgrGazetteerMatching):
