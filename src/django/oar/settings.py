5c3ba70e7eae9a2705a425cbdec5731f4783184b
Michael Maurizi <mmaurizi@azavea.com>
2022-07-06 12:33:23 -0400

    Support breaking changes in django-spa

--- src/django/oar/settings.py
+++ src/django/oar/settings.py
@@ -303,6 +303,7 @@ NOTIFICATION_EMAIL_TO = os.getenv(
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = ((os.path.join(STATIC_ROOT, "static")),)
 
 STATICFILES_STORAGE = 'spa.storage.SPAStaticFilesStorage'
 
