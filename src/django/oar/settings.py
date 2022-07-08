b60f4f261217afc15ca00f25309a043037c8ae88
Michael Maurizi <mmaurizi@azavea.com>
2022-07-07 20:00:32 -0400

    Add setting to continue using AutoField for IDs

--- src/django/oar/settings.py
+++ src/django/oar/settings.py
@@ -223,6 +223,9 @@ DATABASES = {
     }
 }
 
+# Use < 3+ default for ID fields
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
