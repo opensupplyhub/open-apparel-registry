c42d0d922cf9000b3b5079960525af3e1b78703d
Michael Maurizi <mmaurizi@azavea.com>
2022-07-18 12:05:44 -0400

    Update header color from green to yellow

--- src/app/src/util/COLOURS.js
+++ src/app/src/util/COLOURS.js
@@ -6,7 +6,6 @@ export default {
     LIGHT_GREY: '#A5A5A5',
     CIRCLE_COLOR: '#0427a4',
     CIRCLE_COLOR_SHADOW: 'rgba(61, 50, 138, 0.5)',
-    TEAL: '#5ffbce',
-    GREEN: '#62CC74',
+    NAVIGATION: '#FCCF3F',
     NEAR_BLACK: '#0D1128',
 };
