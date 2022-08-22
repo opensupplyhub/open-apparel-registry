5a92a5e5f8bcab044750235c5ff16da041c183ef
Claude M. Schrader <cschrader@azavea.com>
2022-08-22 16:23:46 -0400

    Update the train_model management command
    We're updating this management command to train a new dedupe model based
    on the new architecture. Separating this out into a management command enables
    us to train a model on a scheduled basis.

--- src/django/api/management/commands/train_model.py
+++ src/django/api/management/commands/train_model.py
@@ -1,9 +1,11 @@
 from django.core.management.base import BaseCommand
 
-from api.matching import GazetteerCache
+from api.matching import train_gazetteer, get_model_data
 
 class Command(BaseCommand):
     help = 'Trains a new dedupe model'
 
     def handle(self, *args, **options):
-        GazetteerCache._rebuild_gazetteer(train_model=True)
+        messy, canonical = get_model_data()
+
+        train_gazetteer(messy, canonical)
