8c08548f75f304edc0a23730f2098a707ece27ca
Justin Walgran <jwalgran@azavea.com>
2022-08-26 15:14:52 -0700

    Add facility insert SQL output to makedata

--- src/django/api/management/commands/makedata.py
+++ src/django/api/management/commands/makedata.py
@@ -2,6 +2,8 @@ import csv
 import os
 import random
 from django.core.management.base import BaseCommand
+from django.utils import timezone
+from api.oar_id import make_oar_id
 
 
 # Taken from https://stackoverflow.com/a/312464
@@ -58,10 +60,7 @@ class Command(BaseCommand):
         file_count = options['filecount']
         row_count = options['rowcount']
         out_dir = options['outdir']
-        output_sql = options['sql']
-
-        if output_sql:
-            raise Exception('NOT YET IMPLEMENTED')
+        make_sql = options['sql']
 
         random.seed(8675309)
 
@@ -78,6 +77,7 @@ class Command(BaseCommand):
                     sector = 'Apparel'
                 rows.append(
                     {
+                        'oar_id': make_oar_id(row['country_code']),
                         'country': clean(row['country_code']),
                         'name': clean(row['name']),
                         'address': clean(row['address']),
@@ -96,11 +96,37 @@ class Command(BaseCommand):
             max_file_row = len(rows) - 1
             for _ in range(len(rows), row_count):
                 new_row = rows[random.randint(0, max_file_row)].copy()
+                new_row['oar_id'] = make_oar_id(new_row['country_code'])
                 new_row['name'] = shuffle_words(new_row['name'])
                 new_row['address'] = shuffle_words(new_row['address'])
                 rows.append(new_row)
 
         random.shuffle(rows)
+
+        if make_sql:
+            # TODO: Use COPY rather than INSERT
+            now = timezone.now().isoformat()
+            facility_insert = (
+                "INSERT INTO api_facility "
+                "(id, name, address, country_code, location, created_at, updated_at, created_from_id, has_inexact_coordinates) "
+                "VALUES ('{oar_id}', '{name}', '{address}', '{country}', {location}, '{created_at}', '{updated_at}', {created_from_id}, '{has_inexact_coordinates}')\n")
+            def prepare(rows):
+                for row in rows:
+                    new_row = row.copy()
+                    for col in ('name', 'address'):
+                        new_row[col] = new_row[col].replace("'", "''")
+                    new_row['location'] = 'ST_SetSRID(ST_POINT({lng}, {lat}),4326)'.format(**new_row)
+                    new_row['created_at'] = now
+                    new_row['updated_at'] = now
+                    new_row['has_inexact_coordinates'] = 'f'
+                    # TODO FIX LIST ITEM ID GENERATION
+                    new_row['created_from_id'] = '1'
+                    yield new_row
+            inserts = [facility_insert.format(**row) for row in prepare(rows)]
+            out_file = os.path.join(out_dir, 'facilities.sql')
+            with open(out_file, 'w') as f:
+                f.writelines(inserts)
+            return
 
         chunk_size = len(rows) // file_count
         for i, chunk in enumerate(chunks(rows, chunk_size)):
