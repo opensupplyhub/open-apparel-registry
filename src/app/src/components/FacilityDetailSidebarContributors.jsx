39b0d2c26c1dddd4db842513693dce1f9f6f7a72
Tai Wilkin-Corraggio <twilkin@azavea.com>
2022-09-06 15:37:28 -0400

    Update contributor UX

--- src/app/src/components/FacilityDetailSidebarContributors.jsx
+++ src/app/src/components/FacilityDetailSidebarContributors.jsx
@@ -1,34 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
+import Button from '@material-ui/core/Button';
 
 import ShowOnly from './ShowOnly';
 import BadgeVerified from './BadgeVerified';
+import GroupIcon from './GroupIcon';
+import TitledDrawer from './TitledDrawer';
 
 import { makeProfileRouteLink } from '../util/util';
 
 const detailsSidebarStyles = theme =>
     Object.freeze({
-        item: {
-            paddingTop: '12px',
+        root: {
+            color: '#191919',
+            display: 'flex',
+            justifyContent: 'center',
+            borderTop: '2px solid #F9F7F7',
+            paddingTop: theme.spacing.unit * 3,
         },
-        label: {
-            padding: '12px 24px 6px 24px',
-            fontSize: '0.75rem',
-            textTransform: 'uppercase',
-            fontWeight: theme.typography.fontWeightMedium,
+        contentContainer: {
+            width: '100%',
+            maxWidth: '1072px',
+            paddingLeft: theme.spacing.unit * 3,
+        },
+        button: {
+            fontWeight: 500,
+            fontSize: '18px',
+            lineHeight: '21px',
+            textTransform: 'none',
+            padding: 0,
+        },
+        buttonText: {
+            paddingLeft: theme.spacing.unit,
         },
         primaryText: {
             wordWrap: 'break-word',
         },
+        link: {
+            color: theme.palette.primary.main,
+            textDecoration: 'none',
+        },
         secondaryText: {
-            color: 'rgba(0, 0, 0, 0.54)',
-            display: 'flex',
-            alignItems: 'center',
-            fontSize: '12px',
-            justify: 'flex-end',
+            fontWeight: 500,
+            fontSize: '18px',
+            lineHeight: '21px',
+            color: '#010204',
+            paddingTop: theme.spacing.unit,
         },
         icon: {
             color: 'rgb(106, 106, 106)',
@@ -37,11 +56,15 @@ const detailsSidebarStyles = theme =>
             textAlign: 'center',
         },
         contributor: {
-            boxShadow: '0px -1px 0px 0px rgb(240, 240, 240)',
+            boxShadow: '0px 1px 0px 0px rgb(240, 240, 240)',
+            paddingTop: theme.spacing.unit,
+            paddingBottom: theme.spacing.unit,
         },
     });
 
-const FacilityDetailSidebarContributors = ({ classes, contributors, push }) => {
+const FacilityDetailSidebarContributors = ({ classes, contributors }) => {
+    const [isOpen, setIsOpen] = useState(false);
+
     const visibleContributors = contributors.filter(
         c => !!c.contributor_name || !!c.name,
     );
@@ -49,35 +72,61 @@ const FacilityDetailSidebarContributors = ({ classes, contributors, push }) => {
     if (!visibleContributors.length) return null;
 
     return (
-        <div className={classes.item}>
-            <Typography className={classes.label}>Contributors</Typography>
-            {visibleContributors.map(contributor => (
-                <ListItem
-                    button={!!contributor.id}
-                    onClick={() => {
-                        if (!contributor.id) return;
-                        push(makeProfileRouteLink(contributor.id));
-                    }}
-                    key={`${contributor.id} ${contributor.list_name}`}
-                    className={classes.contributor}
+        <div className={classes.root}>
+            <div className={classes.contentContainer}>
+                <Button
+                    color="primary"
+                    className={classes.button}
+                    onClick={() => setIsOpen(true)}
                 >
-                    <ShowOnly when={contributor.is_verified}>
-                        <BadgeVerified />
-                    </ShowOnly>
-                    <ListItemText
-                        primary={
-                            contributor.contributor_name || contributor.name
-                        }
-                        secondary={contributor.list_name}
-                        classes={{ primary: classes.primaryText }}
-                    />
-                    <ShowOnly when={!!contributor.id}>
-                        <i
-                            className={`${classes.icon} far fa-fw fa-angle-right`}
-                        />
-                    </ShowOnly>
-                </ListItem>
-            ))}
+                    <GroupIcon />{' '}
+                    <span className={classes.buttonText}>
+                        {visibleContributors.length}{' '}
+                        {visibleContributors.length === 1
+                            ? 'organization has'
+                            : 'organizations have'}{' '}
+                        contributed data for this facility
+                    </span>
+                </Button>
+            </div>
+            <TitledDrawer
+                title={
+                    <>
+                        <GroupIcon />{' '}
+                        <span className={classes.buttonText}>
+                            Contributors ({visibleContributors.length})
+                        </span>
+                    </>
+                }
+                open={isOpen}
+                onClose={() => setIsOpen(false)}
+            >
+                {visibleContributors.map(contributor => (
+                    <div
+                        key={`${contributor.id} ${contributor.list_name}`}
+                        className={classes.contributor}
+                    >
+                        <ShowOnly when={contributor.is_verified}>
+                            <BadgeVerified />
+                        </ShowOnly>
+                        {contributor.id ? (
+                            <Link
+                                to={makeProfileRouteLink(contributor.id)}
+                                className={`${classes.primaryText} ${classes.link}`}
+                            >
+                                {contributor.contributor_name ||
+                                    contributor.name}
+                            </Link>
+                        ) : (
+                            <Typography className={classes.primaryText}>
+                                {contributor.contributor_name ||
+                                    contributor.name}
+                            </Typography>
+                        )}
+                        <Typography>{contributor.list_name}</Typography>
+                    </div>
+                ))}
+            </TitledDrawer>
         </div>
     );
 };
