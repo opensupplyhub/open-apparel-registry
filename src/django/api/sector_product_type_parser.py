0790ef94521193e17cf71be47be85575ab85adb6
Matt Stone <mstone@azavea.com>
2022-09-22 20:25:55 -0500

    Add SectorCache to prevent Sector refetching
    Saving the sectors as a static class attribute would require a restart
    of the web service if the Sectors were updated.

--- src/django/api/sector_product_type_parser.py
+++ src/django/api/sector_product_type_parser.py
@@ -1,30 +1,48 @@
+from datetime import datetime, timedelta
 from api.models import Sector
 from api.constants import CsvHeaderField
 
 
-class SectorProductTypeParser:
-    sector_names = [sector.name for sector in Sector.objects.all()]
-    lower_case_sector_names = [name.lower() for name in sector_names]
+class SectorCache:
+    REFRESH_INTERVAL = timedelta(seconds=300)
 
-    csv_fields = [
-        CsvHeaderField.SECTOR,
-        CsvHeaderField.PRODUCT_TYPE,
-        CsvHeaderField.SECTOR_PRODUCT_TYPE
-    ]
+    def __init__(self) -> None:
+        self.map = None
+
+    def refetch_sectors(self):
+        self.map = {
+            sector.name.lower(): sector.name
+            for sector in Sector.objects.all()
+        }
+        self.fetch_time = datetime.now()
+
+    def refresh_if_needed(self):
+        if self.map is None or (
+            datetime.now() > self.fetch_time + SectorCache.REFRESH_INTERVAL
+        ):
+            self.refetch_sectors()
+
+    @property
+    def sector_map(self):
+        self.refresh_if_needed()
+        return self.map
+
+
+class SectorProductTypeParser:
+    sector_cache = SectorCache()
 
     def __init__(self):
         self.sectors = []
         self.product_types = []
 
     def parse_all_values(self, all_values):
+        sector_map = SectorProductTypeParser.sector_cache.sector_map
+
         for value in all_values:
-            try:
-                index = SectorProductTypeParser.lower_case_sector_names.index(
-                    value.lower().strip()
-                )
-                self.sectors.append(
-                    SectorProductTypeParser.sector_names[index])
-            except ValueError:
+            clean_value = self.clean_value(value)
+            if (clean_value in sector_map):
+                self.sectors.append(sector_map[clean_value])
+            else:
                 self.product_types.append(value)
 
         if len(self.sectors) == 0:
@@ -33,6 +51,9 @@ class SectorProductTypeParser:
     def sort_values(self):
         self.sectors.sort()
         self.product_types.sort()
+
+    def clean_value(self, value):
+        return value.lower().strip()
 
 
 class RequestBodySectorProductTypeParser(SectorProductTypeParser):
@@ -51,11 +72,17 @@ class RequestBodySectorProductTypeParser(SectorProductTypeParser):
 
 
 class CsvRowSectorProductTypeParser(SectorProductTypeParser):
+    csv_fields = [
+        CsvHeaderField.SECTOR,
+        CsvHeaderField.PRODUCT_TYPE,
+        CsvHeaderField.SECTOR_PRODUCT_TYPE
+    ]
+
     def __init__(self, fields, values):
         super().__init__()
 
         all_values = set()
-        for field in SectorProductTypeParser.csv_fields:
+        for field in CsvRowSectorProductTypeParser.csv_fields:
             if field in fields:
                 value_string = values[fields.index(field)]
                 for value in value_string.split("|"):
