4b88e529b7c942a818a9cc0b08b7d19a03e6ffbe
Matt Stone <mstone@element84.com>
2023-02-27 16:49:58 -0600

    Update UI links with data@opensupplyhub.org email

--- src/app/src/__tests__/utils.tests.js
+++ src/app/src/__tests__/utils.tests.js
@@ -1016,7 +1016,7 @@ it('creates params from a query string', () => {
 
 it('creates an email link for reporting a data issue for a facility with a given OS ID', () => {
     const osID = 'osID';
-    const expectedMatch = 'mailto:info@opensupplyhub.org?subject=Reporting a data issue on ID osID';
+    const expectedMatch = 'mailto:data@opensupplyhub.org?subject=Reporting a data issue on ID osID';
 
     expect(makeReportADataIssueEmailLink(osID)).toBe(expectedMatch);
 });
@@ -1525,7 +1525,7 @@ it('creates an API URL for merging two facilties', () => {
     const toMergeID = 'toMergeID';
 
     const expectedURL =
-          '/api/facilities/merge/?target=targetID&merge=toMergeID';
+        '/api/facilities/merge/?target=targetID&merge=toMergeID';
 
     expect(isEqual(
         expectedURL,
