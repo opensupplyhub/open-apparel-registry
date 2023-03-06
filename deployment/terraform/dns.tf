f27bb980d2c514e2bc0823a8e6ef26ca9f9c6514
Claude M. Schrader <cschrader@azavea.com>
2023-03-06 17:16:37 -0500

    Cleaning up more unnecessary resources
    The VPC, the SNS topic, and the internal hosted zone are all being
    removed, since the resources remaining should not rely on these

--- deployment/terraform/dns.tf
+++ deployment/terraform/dns.tf
@@ -1,21 +1,4 @@
 #
-# Private DNS resources
-#
-resource "aws_route53_zone" "internal" {
-  name = "${var.r53_private_hosted_zone}"
-
-  vpc {
-    vpc_id     = "${module.vpc.id}"
-    vpc_region = "${var.aws_region}"
-  }
-
-  tags {
-    Project     = "${var.project}"
-    Environment = "${var.environment}"
-  }
-}
-
-#
 # Public DNS resources
 #
 resource "aws_route53_zone" "external" {
